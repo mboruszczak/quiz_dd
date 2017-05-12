faefe902f32d25e564f80645b8578b20e805a725
mboruszczak <mboruszczak@deloittece.com>
2017-05-12 14:23:04 +0200

    working on summary

--- app/Quiz.php
+++ app/Quiz.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\DB;
 class Quiz extends Model
 {
     
+    public $user_answers_array = [];
+    public $matrix_answers_array = [];
     
     public function startQuiz($quiz_id) {
         
@@ -84,24 +86,65 @@ class Quiz extends Model
                 ->select('question_id', 'answer')
                 ->where('user_id', $user_id)
                 ->where('quiz_id', $quiz_id)
+                ->orderBy('question_id', 'asc')
                 ->get();
         
-
         foreach($user_answers as $user_answer) {
             $temp = unserialize($user_answer->answer);
-            $answers_array = [
-                $user_answer->question_id => $temp,
-            ];
+            $this->user_answers_array[$user_answer->question_id] = $temp;
         }
-        
-        var_dump($answers_array);
         
         
         $treshold = DB::table('quizs')
                 ->select('treshold')
                 ->where('id', $quiz_id)
                 ->first();
-        var_dump("------------<br>".$treshold);
+        
+        $count_points = DB::table('answers')
+                ->select(DB::raw('count(*) as max_points'))
+                ->where('quiz_id', $quiz_id)
+                ->where('correct', 1)
+                ->first();
+        
+        $answers_matrix = DB::table('answers')
+                ->select('question_id', 'id')
+                ->where('quiz_id', $quiz_id)
+                ->where('correct', 1)
+                ->orderBy('question_id', 'asc')
+                ->get();
+        
+        $max_points = $count_points->max_points;
+       
+        
+        
+        
+        
+        
+        $first_lap = true;
+        $temp_id = 0;
+        $temp2 = [];
+        $i = 0;
+        
+        foreach($answers_matrix as $correct_answer) {
+            
+            if($correct_answer->question_id == $temp_id || $first_lap === true) {
+                $temp2[$i] = $correct_answer->id;
+                var_dump($temp2);
+                $i+=1;
+            }
+            else {
+                $this->matrix_answers_array[$correct_answer->question_id] = $temp2;
+                $i = 0;
+                //clear temp2 array
+
+            }
+            
+            $temp_id = $correct_answer->question_id;
+            $first_lap= false;
+        }
+        
+        // var_dump($this->matrix_answers_array);
+
         
         exit();
     }
